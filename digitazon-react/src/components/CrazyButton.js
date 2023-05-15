fcc8000ad06fbd1a4f35515f367068c54419adc1
chandraSaur <morenamasala@gmail.com>
2023-05-15 21:37:10 +0200

    crazy button esercizio

--- digitazon-react/src/components/CrazyButton.js
+++ digitazon-react/src/components/CrazyButton.js
@@ -1,6 +1,27 @@
+//scrivere un pulsante non cliccabile, quando l'utente ci arriva quasi sopra 
+//(come potete programmare questo "quasi"?) il pulsante si sposta in un punto random della pagina.
+
 import {useState, useEffect} from "react"
 import './CrazyButton.css'
 
 export function CrazyButton() {
-    
+    const [position, setPosition] = useState({x: 0, y: 0})
+
+    const newPosition = () => {
+        let newX =  Math.random()*1700
+        let newY = Math.random()*800
+        setPosition({x: newX, y: newY})
+
+    } 
+    const posizione = {
+        position: "absolute",
+        left: position.x,
+        top: position.y
+    }
+
+    return(
+        <div className="spostati" style={posizione} onMouseEnter={newPosition} >
+        <button>Cliccami</button>
+        </div>
+    )
 }
