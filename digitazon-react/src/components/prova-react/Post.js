2bbf49a8776c6368753aab7a8aba11c589279226
chandraSaur <morenamasala@gmail.com>
2023-05-17 11:09:45 +0200

    miscellanous

--- digitazon-react/src/components/prova-react/Post.js
+++ digitazon-react/src/components/prova-react/Post.js
@@ -8,18 +8,18 @@ export default function Post({number}){
    
    useEffect (()=>{
       async function asyncCall() {
-         const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+         const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${number}`)
          const json = await res.json()
-         setPost(json.slice((parseInt(number)-1), parseInt(number)))
+         setPost(json) //.slice((parseInt(number)-1), parseInt(number)))
      }
      asyncCall()
    }, [])
 
    useEffect (()=>{
       async function asyncCallComments() {
-         const result = await fetch('https://jsonplaceholder.typicode.com/comments')
+         const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${number}/comments`)
          const json2 = await result.json()
-         setComments(json2.filter((specificPost) => specificPost.postId == number))
+         setComments(json2) //.filter((specificPost) => specificPost.postId == number))
      }
      asyncCallComments()
    }, [])
@@ -27,14 +27,10 @@ export default function Post({number}){
 
    return(
       <div className="post">
-         {
-            post.map((p)=>
-               <div className="main-post">
-                  <h1 key={p.id}>{p.title}</h1>
-                  <p>"{p.body}"</p>
-               </div>
-            )
-         }
+         <div className="main-post">
+            <h1 key={post.id}>{post.title}</h1>
+            <p>"{post.body}"</p>
+         </div>
          {
             comments.map((c) =>
             <div className="comments">
